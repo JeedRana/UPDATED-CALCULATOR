e9fcea4d86828a8be4f6b93d92a1be0c289df845
JeedRana <150332461+JeedRana@users.noreply.github.com>
2023-11-10 11:26:19 +0800

    Add files via upload

---
+++ Rana_Cal/src/main/java/com/mycompany/rana_cal/Rana_Cal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rana_cal;
+
+/**
+ *
+ * @author WS
+ */
+public class Rana_Cal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
